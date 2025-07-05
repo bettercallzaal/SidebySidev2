2d07b7209cd03c90aa152affbd7833fd38c2e493
Zaal Panthaki <zaalpanthaki@Mac.lan>
2025-07-04 21:23:02 -0400

    Disable CSS optimization to fix critters module error

--- next.config.js
+++ next.config.js
@@ -3,9 +3,8 @@ const nextConfig = {
   reactStrictMode: false,
   // Remove swcMinify as it's causing warnings
   experimental: {
-    // This helps with some hydration issues by prioritizing client rendering
-    // for components that use browser APIs
-    optimizeCss: true,
+    // Disabled CSS optimization due to missing critters module
+    // optimizeCss: true,
     scrollRestoration: true,
   },
   // Disable TypeScript type checking during build
